36e4f62e8db0f3af33334153e3032311456fd859
letu <letu071299@gmail.com>
2022-02-09 10:30:26 +0700

    new

--- Video.py
+++ Video.py
@@ -1,24 +1,34 @@
 import cv2
 import numpy as np
 import time
+import os
 
 net = cv2.dnn.readNet("yolov4-tiny-custom_best.weights","yolov4-tiny-custom.cfg")
 
 classes = ["NoFaceMask","WearFaceMask"]
-
-#cap = cv2.VideoCapture("Data Test/videotest.mp4")
+font = cv2.FONT_HERSHEY_COMPLEX_SMALL
+colors = [(0, 0, 255), (0, 255, 0)]
+cap = cv2.VideoCapture("Data Test/TestFaceMask2.mp4")
 #cap = cv2.VideoCapture(0)
-url = "http://192.168.1.4:4747/video"
-cap = cv2.VideoCapture(url)
+
+# url = "http://192.168.1.4:4747/video"
+# cap = cv2.VideoCapture(url)
+
 #fourcc = cv2.VideoWriter_fourcc(*'XVID')
 #out = cv2.VideoWriter('Video-demo-detect-Facemask.avi',fourcc, 20.0, (640,360))
 
 prev_frame_time = 0
 new_frame_time = 0
+path_storage = "Warning/"
 
-while True:
+if not os.path.exists(path_storage): 
+    os.mkdir("Warning")
+
+count = 0
+
+while cap.isOpened():
+	record = False
 	ret, frame = cap.read()
-	
 	if not ret:
 		break
 	
@@ -26,8 +36,9 @@ while True:
 	
 	fps = 1 / (new_frame_time - prev_frame_time)
 	prev_frame_time = new_frame_time
-	
+
 	height, width, _ = frame.shape
+	cv2.rectangle(frame, (0,0), (width, 50), (0,255,0), -1)
 	# [blobFromImage] tạo đốm màu 4 chiều từ hình ảnh. Tùy chọn thay đổi kích thước và cắt hình ảnh từ trung tâm, trừ giá trị trung bình,
 	# chia tỷ lệ giá trị theo tỷ lệ, hoán đổi kênh Xanh lam và Đỏ.
 	blob = cv2.dnn.blobFromImage(frame, 1./255, (416,416),(0,0,0),swapRB=True, crop=False)
@@ -39,9 +50,7 @@ while True:
 	# cv2.waitKey(0)
 	net.setInput(blob)
 
-
 	output_layers_names = net.getUnconnectedOutLayersNames()          #['yolo_30', 'yolo_37']
-
 	layerOutputs = net.forward(output_layers_names)                  #2 Ma trận kích thước (507,7) và (2028,7)
 
 	boxes = []
@@ -55,8 +64,8 @@ while True:
 			scores = detection[5:7]  # Chứa confidence của các class trong code này có 2 class
 			class_id = np.argmax(scores)      #Chỉ số của class mà có confidence lớn hơn
 			confidence = scores[class_id]  # confidence: độ tin cậy của boundingbox, nhận giá trị từ 0 đến 1
-			print(confidence)
-			if confidence > 0.5:
+			# print(confidence)
+			if confidence > 0.8:
 				center_x = int(detection[0] * width)        #Tọa độ tâm của bounding box thực tế trên ảnh
 				center_y = int(detection[1] * height)
 				w = int(detection[2] * width)               #Chiều rộng, chiều cao thực tế của bounding box so với kích thước ảnh
@@ -70,16 +79,13 @@ while True:
 				confidences.append((float(confidence)))         #danh sách các confidence cảu các bouding box
 				class_ids.append(class_id)                      #Danh sách chỉ số của class có confidence lớn hơn (nhận giá trị 0 hoặc 1)
 
-		print(boxes)
-		print(confidences)
-		print(class_ids)
+		# print(boxes)
+		# print(confidences)
+		# print(class_ids)
 
 		# Chọn ra những box có độ tin cậy cao, đại diện box là chỉ số của class
 		indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)            #0.5: Ngưỡng lọc confidence tương ứng với box, để loại box
-		print("Indexes: ",indexes)
-		font = cv2.FONT_HERSHEY_COMPLEX_SMALL
-		colors = [(0, 0, 255), (0, 255, 0)]
-
+		# print("Indexes: ",indexes)
 
 		for i in range(len(boxes)):          #len(boxes) = 2
 			if i in indexes:
@@ -88,12 +94,17 @@ while True:
 				confidence = str(round(confidences[i], 2))
 				color = colors[class_ids[i]]
 				cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
-
 				cv2.putText(frame, label + " " + confidence, (x, y - 5), font, 1, color, 2)
-	
-	cv2.putText(frame, "FPS: "+str(int(fps)), (30,30), font, 2, (0,255,0),2)
+				if count%20==0 and label=="NoFaceMask":
+					record = True
+	current_time = time.ctime(time.time())
+	cv2.putText(frame, "FPS: "+str(int(fps)), (10,20), font, 0.8, (255,0,0),2)
+	cv2.putText(frame, current_time, (10,40), font, 0.8, (255,0,0),2)
 	cv2.imshow('Image', frame)
+	count += 1
 #	out.write(frame)
+	if record:
+		cv2.imwrite(path_storage+current_time+".jpg", frame)
 
 	if cv2.waitKey(1) == ord("q"):
 		break
